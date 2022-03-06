ce1a3f3e66a81a21ae96db8b88500327e0f6c3b3
Trevor Stahl <trevorclarkstahl@gmail.com>
2022-03-05 20:16:49 -0700

    basic functionallity working

--- analyses.py
+++ analyses.py
@@ -1,2 +1,125 @@
-def runComp(data):
-    return "from run comp"
+from flask import Flask, request, jsonify
+
+
+def singleAnalysis(metabolite, level):
+    if str(metabolite) == "albumin":
+        result = 35 < level < 55
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "bun":
+        result = 80 < level < 200
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "calcium":
+        result = 86 < level < 102
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "co2":
+        result = 23 < level < 28
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "chloride":
+        result = 98 < level < 106
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "creatine":
+        result = 5 < level < 13
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "glucose":
+        result = 700 < level < 990
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "potassium":
+        result = 35 < level < 50
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "sodium":
+        result = 136 < level < 145
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "total-bilirubin":
+        result = 3 < level < 10
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "total-protein":
+        result = 55 < level < 90
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "alt":
+        result = 10 < level < 40
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "alp":
+        result = 30 < level < 120
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    elif str(metabolite) == "ast":
+        result = 10 < level < 40
+        if result:
+            result = "IN reference range"
+        else:
+            result = "OUT reference range"
+        response = jsonify(metabolite, level, result)
+        return response
+    else:
+        response = f"Sorry, we do not currently support {metabolite} as a metabolite, yet"
+        return response
+
+
+def comprehensive():
+    response = jsonify(albumin="within limits", bun="above recommended limit")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, request, jsonify
-from markupsafe import escape
+from flask import render_template
 
 from analyses import *
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Welcome to The Comprehensive Metabolic Panel API'
+def index():  # put application's code here
+    return render_template("index.html")
 
 
-@app.route("/comp")
-def comprehensive():
-    return runComp(request.data)
+@app.route("/1metabolite/<metabolite>/<level>", methods=["GET"])
+def singleMetabolite(metabolite, level):
+    try:
+        checked_level = int(level)
+        return singleAnalysis(metabolite, checked_level)
+    except ValueError:
+        return "Your level value doesn't seem to be a number"
 
 
 if __name__ == '__main__':
